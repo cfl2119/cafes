b8d7da072314aa137e3116ac635fd814fb36c550
Connie <connieleeabc@gmail.com>
2012-03-25 21:33:40 -0400

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Cafes::Application.routes.draw do
+  
+  get "home/index"
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +54,8 @@ Cafes::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+    root :to => 'home#index'
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
