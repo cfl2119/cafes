147871ff51901ba61e1ae90b472354b27ef6d310
Connie <connieleeabc@gmail.com>
2012-04-15 18:57:24 -0400

    user attr_accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-
+  attr_accessible :name, :email, :password, :password_confirmation
   validates :email, :uniqueness => { :message => "emails should be unique per user!" }
   has_secure_password
   validates_presence_of :password, :on => :create
