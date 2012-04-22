69618b8c1cb550b68b67caa61ab4c8a054fcf5e4
Connie <connieleeabc@gmail.com>
2012-04-22 19:03:23 -0400

    hwk 5 update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  before_filter :current_user
-  skip_before_filter :check_session, :only => [:new, :create]
 
+  skip_before_filter :check_session, :only => [:new, :create]
+  before_filter :current_user
+ 
   force_ssl
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,7 @@ class UsersController < ApplicationController
   # GET /users.json
 
   
-
-  before_filter :check_session
+  before_filter :check_session, :except => :new
 
   def check_session 
     if session[:user_id] ==nil
