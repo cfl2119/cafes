597099493828adcb1dc8f58d5cec6c02f89c088f
Connie <connieleeabc@gmail.com>
2012-04-22 17:05:39 -0400

    homework 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :current_user
   skip_before_filter :check_session, :only => [:new, :create]
 
   force_ssl
